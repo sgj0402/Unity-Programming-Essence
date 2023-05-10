27f6fbe8ceec38c37fe8f916f771be7e56c1ff4b
sgj0402 <sgj0402@gmail.com>
2023-05-10 22:15:41 +0900

    create PlayerController code

---
+++ Dodge/Assets/PlayerController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody playerRigidbody;
+    public float speed = 8f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
