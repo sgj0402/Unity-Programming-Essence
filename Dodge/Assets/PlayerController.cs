7c12f5293779e77da403aa5010e312c0e8c11eb9
sgj0402 <sgj0402@gmail.com>
2023-05-10 22:24:06 +0900

    detect user input

--- Dodge/Assets/PlayerController.cs
+++ Dodge/Assets/PlayerController.cs
@@ -7,13 +7,22 @@ public class PlayerController : MonoBehaviour
     public Rigidbody playerRigidbody;
     public float speed = 8f;
 
-    void Start()
-    {
+    void Start() {
         
     }
 
-    void Update()
-    {
-        
+    void Update() {
+        if (Input.GetKey(KeyCode.UpArrow) == true) {
+            playerRigidbody.AddForce(0f, 0f, speed);
+        }
+        if (Input.GetKey(KeyCode.DownArrow) == true) {
+            playerRigidbody.AddForce(0f, 0f, -speed);
+        }
+        if (Input.GetKey(KeyCode.RightArrow) == true) {
+            playerRigidbody.AddForce(speed, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) == true) {
+            playerRigidbody.AddForce(-speed, 0f, 0f);
+        }
     }
 }
