e0787152c354c327b62d793c286e59063ed9b6eb
sgj0402 <sgj0402@gmail.com>
2023-05-11 16:29:01 +0900

    Bullet OnTriggerEnter() to kill Player

--- Dodge/Assets/Bullet.cs
+++ Dodge/Assets/Bullet.cs
@@ -13,4 +13,14 @@ public class Bullet : MonoBehaviour
 
         Destroy(gameObject, 3f);
     }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+            PlayerController playerController = other.GetComponent<PlayerController>();
+
+            if (playerController != null) {
+                playerController.Die();
+            }
+        }
+    }
 }
