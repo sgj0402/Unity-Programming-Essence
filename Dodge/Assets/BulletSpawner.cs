5eec415bae1b6b14aacdd80f3ee8b07f842fab52
sgj0402 <sgj0402@gmail.com>
2023-05-11 17:11:40 +0900

    spawn bullet

--- Dodge/Assets/BulletSpawner.cs
+++ Dodge/Assets/BulletSpawner.cs
@@ -19,6 +19,15 @@ public class BulletSpawner : MonoBehaviour
     }
 
     void Update() {
-        
+        timeAfterSpawn += Time.deltaTime;
+
+        if (timeAfterSpawn >= spawnRate) {
+            timeAfterSpawn = 0f;
+
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+            bullet.transform.LookAt(target);
+
+            spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+        }
     }
 }
